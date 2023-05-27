0e333acc54d929e6e8123c61f915a03c5c31aa37
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-05-27 19:25:35 -0300

    se agregan los campos del formulario: nombre, apellido, dni e email

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,29 @@
-import { Form } from "react-bootstrap";
+import { Form, Col, Row, Button } from "react-bootstrap";
 const Formulario = () => {
     return(
-        <Form>
-            
-        </Form>
+        <Form id="formDatos">
+        <Row>
+          <Col md={6} className="mt-2">
+            <Form.Label>Nombre *</Form.Label>
+            <Form.Control type="text" placeholder="María Lourdes" required minLength={3} maxLength={50}/>
+          </Col>
+          <Col md={6} className="mt-2">
+            <Form.Label>Apellido</Form.Label>
+            <Form.Control type="text" placeholder="García" required minLength={3} maxLength={50} />
+          </Col>
+          <Col md={6} className="mt-2">
+            <Form.Label>DNI</Form.Label>
+            <Form.Control type="number" placeholder="41061228" required minLength={3} maxLength={9}/>
+          </Col>
+          <Col md={6} className="mt-2">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="lourdesgarciafyl@gmail.com" required minLength={3} maxLength={50} />
+          </Col>
+        </Row>
+        <Button className="mt-3" variant="primary" type="submit">
+        Enviar
+      </Button>
+      </Form>
     )
 }
 
