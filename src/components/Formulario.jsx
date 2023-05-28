d6369b5ce6afcb9f6e408f53ce718aedf797ad4e
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-05-28 15:28:37 -0300

    se agrega el handleSubmit y el register, agregando el register tambien en cada campo

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,37 +1,65 @@
 import { Form, Col, Row, Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 const Formulario = () => {
+    const { register, handleSubmit} = useForm()
+  
     const enviarFormulario = () => {
 
     }
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const apretarEnviar = (data) => {
+      console.log(data)
+      reset()
     }
+
     return(
-        <Form id="formDatos" onSubmit={handleSubmit}>
+        <Form id="formDatos" onSubmit={handleSubmit(apretarEnviar)}>
         <Row>
           <Col md={6} className="mt-2">
             <Form.Group controlId="validaronNombre">
             <Form.Label>Nombre *</Form.Label>
-            <Form.Control type="text" placeholder="María Lourdes" required minLength={3} maxLength={50}/>
+            <Form.Control 
+            type="text" 
+            placeholder="María Lourdes" 
+            required 
+            minLength={3} 
+            maxLength={50}
+            {...register(`nombre`)}/>
             </Form.Group>
           </Col>
           <Col md={6} className="mt-2">
             <Form.Group controlId="validarApellido">
             <Form.Label>Apellido</Form.Label>
-            <Form.Control type="text" placeholder="García" required minLength={3} maxLength={50} />
+            <Form.Control 
+            type="text" 
+            placeholder="García" 
+            required 
+            minLength={3} 
+            maxLength={50}
+            {...register(`apellido`)} />
             </Form.Group>
           </Col>
           <Col md={6} className="mt-2">
             <Form.Group controlId="validarDni">
             <Form.Label>DNI</Form.Label>
-            <Form.Control type="number" placeholder="41061228" required minLength={3} maxLength={9}/>
+            <Form.Control 
+            type="number" 
+            placeholder="41061228" 
+            required 
+            minLength={3} 
+            maxLength={9}
+            {...register(`dni`)}/>
             </Form.Group>
           </Col>
           <Col md={6} className="mt-2">
             <Form.Group controlId="validarEmail">
             <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="lourdesgarciafyl@gmail.com" required minLength={3} maxLength={50} />
+            <Form.Control 
+            type="email" 
+            placeholder="lourdesgarciafyl@gmail.com" 
+            required 
+            minLength={3} 
+            maxLength={50} 
+            {...register(`email`)}/>
             </Form.Group>
           </Col>
         </Row>
