327a747310698c8ce0c9d0cbcb21c73734da4c96
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-05-28 15:15:27 -0300

    agregamos en cada item un form control e instalamos el react-hook-form para trabajar con el formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,5 @@
 import { Form, Col, Row, Button } from "react-bootstrap";
-
+import { useForm } from "react-hook-form";
 const Formulario = () => {
     const enviarFormulario = () => {
 
@@ -11,20 +11,28 @@ const Formulario = () => {
         <Form id="formDatos" onSubmit={handleSubmit}>
         <Row>
           <Col md={6} className="mt-2">
+            <Form.Group controlId="validaronNombre">
             <Form.Label>Nombre *</Form.Label>
             <Form.Control type="text" placeholder="María Lourdes" required minLength={3} maxLength={50}/>
+            </Form.Group>
           </Col>
           <Col md={6} className="mt-2">
+            <Form.Group controlId="validarApellido">
             <Form.Label>Apellido</Form.Label>
             <Form.Control type="text" placeholder="García" required minLength={3} maxLength={50} />
+            </Form.Group>
           </Col>
           <Col md={6} className="mt-2">
+            <Form.Group controlId="validarDni">
             <Form.Label>DNI</Form.Label>
             <Form.Control type="number" placeholder="41061228" required minLength={3} maxLength={9}/>
+            </Form.Group>
           </Col>
           <Col md={6} className="mt-2">
+            <Form.Group controlId="validarEmail">
             <Form.Label>Email</Form.Label>
             <Form.Control type="email" placeholder="lourdesgarciafyl@gmail.com" required minLength={3} maxLength={50} />
+            </Form.Group>
           </Col>
         </Row>
         <Button className="mt-3" variant="primary" type="submit" onClick={enviarFormulario}>
